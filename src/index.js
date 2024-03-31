9d844d4e4b5836e8aa36fb4333b5b7c9bfa4c356
eraykeskinmac <eraykeskinmac@gmail.com>
2024-03-31 16:01:19 +0300

    feat: add @cloudflare/ai as a dependency and integrate AI tasks in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Ai } from './vendor/@cloudflare/ai.js';
+
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env) {
+    const tasks = [];
+    const ai = new Ai(env.AI);
+
+
+    // prompt - simple completion style input
+    let simple = {
+      prompt: 'Tell me a joke about Cloudflare'
+    };
+    let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', simple);
+    tasks.push({ inputs: simple, response });
+
+
+    // messages - chat style input
+    let chat = {
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: 'Who won the world series in 2020?' }
+      ]
+    };
+    response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
+    tasks.push({ inputs: chat, response });
+
+
+    return Response.json(tasks);
+  }
 };
+
